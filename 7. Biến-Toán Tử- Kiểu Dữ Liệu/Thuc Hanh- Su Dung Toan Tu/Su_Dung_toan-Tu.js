b1d2639616e1a68c5320856300878cdc9ed1dbad
dh.long10 <dh.long10@gmail.com>
2024-07-13 20:53:26 +0700

    bai tap

---
+++ 7. Biến-Toán Tử- Kiểu Dữ Liệu/Thuc Hanh- Su Dung Toan Tu/Su_Dung_toan-Tu.js
@@ -0,0 +1,8 @@
+let inputwidth;
+let inputheigh;
+inputwidth = prompt(" nhập chiều rộng:");
+inputheigh = prompt(" nhập chiều dài: ");
+let width=parseInt(inputwidth)
+let height= parseInt(inputheigh)
+let area = width * height;
+document.write("Chu Vi ="+ area);
